15ca4d82e46bf7592255a9e7fc6a9ec586f7516f
Marcia Goetsch <marcia.goetsch@channing.harvard.edu>
2016-05-29 00:37:37 -0400

    process video as well as stills

---
+++ artistic-transfer.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+sudo apt-get install --force-yes -y libav-tools
+gunzip DeepFlow_release2.0.tar.gz
+tar xvf DeepFlow_release2.0.tar
+unzip deepmatching_1.2.1.zip
+cd /home/ubuntu
+git clone https://github.com/manuelruder/artistic-videos.git
+mv /vagrant/DeepFlow_release2.0/deepflow2-static artistic-videos
+mv /vagrant/deepmatching_1.2.1_c++/deepmatching-static artistic-videos
+cd artistic-videos/models
+sh download-models.sh
